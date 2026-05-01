51fb78e150f132a9509ac2f275007d6cd329766c
colebeebe <bee14003@byui.edu>
2026-05-01 19:37:19 -0400

    Testing a theory

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,13 +4,11 @@ const connectMongo = require('../db/connection');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    connectMongo('cse341');
     const contacts = await Contact.find();
     res.json(contacts);
 });
 
 router.get('/:id', async (req, res) => {
-    connectMongo('cse341');
     const contact = await Contact.findById(req.params.id);
     res.json(contact);
 });
