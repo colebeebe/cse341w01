d1b5e77a01b9af699074a2ca15a86047d061c749
colebeebe <bee14003@byui.edu>
2026-05-01 19:32:28 -0400

    Maybe this will work

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const Contact = require('../db/contact');
+const connectMongo = require('../db/connection');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    connectMongo('cse341');
     const contacts = await Contact.find();
     res.json(contacts);
 });
 
 router.get('/:id', async (req, res) => {
+    connectMongo('cse341');
     const contact = await Contact.findById(req.params.id);
     res.json(contact);
 });
