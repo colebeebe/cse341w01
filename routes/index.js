e37511e418638a26d3e800495f539eba67198749
colebeebe <bee14003@byui.edu>
2026-05-15 13:21:02 -0400

    Moved docs to root once again

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('<a href="/api-docs">View Docs</a>');
-});
-
-router.use('/api-docs', require('./swagger'));
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
-
 
 module.exports = router;
